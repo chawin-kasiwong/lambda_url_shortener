a9c62b2563dfbce3c2f5b3a94c645c06461a86af
Chawin <chawin.kasiwong@gmail.com>
2025-03-07 18:15:37 +0000

    add README instructions

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module lambda_url_shortener
 
-go 1.23.2
+go 1.24.1
 
 require (
 	github.com/aws/aws-lambda-go v1.47.0
--- internal/dto/request.go
+++ internal/dto/request.go
@@ -2,5 +2,5 @@ package dto
 
 type ShortenRequest struct {
 	LongURL string `json:"long_url" validate:"required,url"`
-	Expiry  int64  `json:"expiry",omitempty"`
+	Expiry  int64  `json:"expiry,omitempty"`
 }
